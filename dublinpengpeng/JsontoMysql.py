03f68574d72033f3762eaea1fcf43cc9a16eadc7
RubyJ <xi.jiang@ucdconnect.ie>
2020-02-24 14:10:55 +0000

    rewrite the file JsontoMysql.py add timestamp to dynamic station data stored change the original JsontoMysql.py to Fetch_Json_To_Database.py

--- dublinpengpeng/JsontoMysql.py
+++ dublinpengpeng/JsontoMysql.py
@@ -2,61 +2,124 @@
 # coding: utf-8
 
 
-
+import os
 import json
 import pandas as pd
 import pymysql
+import sqlalchemy as sqla
+from sqlalchemy import create_engine
+import requests
+import traceback
+import datetime
+import time
+
+URI = "dublinbike.cx8vz93uf5cv.eu-west-1.rds.amazonaws.com"
+PORT = "3306"
+DB = "dublinbike"
+USER = "admin"
+PASSWORD = "pengpeng"
+
+SQL_CREATE_DB = "CREATE DATABASE IF NOT EXISTS dublinbike;"
+
+SQL_CREATE_STATIC = '''
+CREATE TABLE IF NOT EXISTS station(
+address VARCHAR(256),
+banking INTEGER,
+bike_stands INTEGER,
+bonus INTEGER,
+contract_name VARCHAR(256),
+name VARCHAR(256),
+number INTEGER,
+position_lat REAL,
+position_lng REAL,
+status VARCHAR(256)
+)'''
+
+SQL_CREATE_DYNAMIC = '''
+CREATE TABLE IF NOT EXISTS availability(
+datetime VARCHAR(256),
+number INTEGER, 
+available_bikes INTEGER, 
+available_bike_stands INTEGER, 
+last_update INTEGER)
+'''
+
+NAME = "Dublin"
+STATIONS = "https://api.jcdecaux.com/vls/v1/stations"
+APIKEY = "30af517cf938432caf25e8fe0fa46fedf2fc6e41"
+
+
+# do validation and checks before insert
+def validate_string(val):
+    if val != None:
+        if type(val) is int:
+            # for x in val:
+            #   print(x)
+            return str(val).encode('utf-8')
+        else:
+            return val
 
 def main():
-    # try:
-    with open('dublin.json') as json_data:
-        json_obj = json.load(json_data)
 
+    engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
+    engine.execute(SQL_CREATE_DB)
 
+    # Create target Directory if don't exist
+    if not os.path.exists("data"):
+        os.mkdir("data")
 
-    # do validation and checks before insert
-    def validate_string(val):
-        if val != None:
-            if type(val) is int:
-                # for x in val:
-                #   print(x)
-                return str(val).encode('utf-8')
-            else:
-                return val
+    def write_to_file(text):
+        with open("data/bikes_{}".format(now).replace(" ", "_"), "w") as f:
+            f.write(text)
 
+    def station_to_db(text):
+        try:
+            res = engine.execute("DROP TABLE IF EXISTS station")
+            res = engine.execute(SQL_CREATE_STATIC)
+            print(res.fetchall())
+        except Exception as e:
+            print(e)
+        stations = json.loads(text)
+        print(type(stations), len(stations))
+        for station in stations:
+            print(station)
+            vals = (station.get('address'), int(station.get('banking')),
+                    station.get('bike_stands'), int(station.get('bonus')),
+                    int(station.get('last_update')), station.get('contract_name'),
+                    int(station.get('number')), station.get('position').get('lat'),
+                    station.get('position').get('lng'), station.get('status'))
+            engine.execute("insert into station values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", vals)
+        return
 
-    # connect to MySQL
-    con = pymysql.connect(host='dublinbike.cx8vz93uf5cv.eu-west-1.rds.amazonaws.com', user='admin', passwd='pengpeng', db='dublinbike')
-    cursor = con.cursor()
+    def availability_to_db(text):
+        try:
+            res = engine.execute(SQL_CREATE_DYNAMIC)
+            print(res.fetchall())
+        except Exception as e:
+            print(e)
+        stations = json.loads(text)
+        print(type(stations), len(stations))
+        for station in stations:
+            print(station)
+            timestampStr = now.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+            vals = (timestampStr, int(station.get('number')), int(station.get('available_bikes')),
+                    int(station.get('available_bike_stands')), int(station.get('bonus')),
+                    )
+            engine.execute("insert into availability values(%s,%s,%s,%s,%s)", vals)
+        return
 
-
-    #     latitude = validate_string(item.get("latitude", None))
-    #     longitude = validate_string(item.get("longitude", None))
-    #     cursor.execute("INSERT INTO dublinbike_static (number, name, address) VALUES (%s,%s,$s)", (number, name, address))
-    #     cursor.execute("INSERT INTO dublinbike_static (number,	name,	address,	latitude,	longitude) VALUES (%s,	%s,	%s,	%s,	%s)", (number,	name,	address,	latitude,	longitude))
-
-
-    cursor.execute(
-        "CREATE TABLE `dublinbike`.`dublinbike_static` ( `number` INT NULL,  `name` VARCHAR(45) NULL,  `address` VARCHAR(45) NULL,  `latitude` VARCHAR(45) NULL,  `longitude` VARCHAR(45) NULL);")
-    # parse json data to SQL insert
-    for i, item in enumerate(json_obj):
-        number = validate_string(item.get("number", None))
-        name = validate_string(item.get("name", None))
-        address = validate_string(item.get("address", None))
-        latitude = validate_string(item.get("latitude", None))
-        longitude = validate_string(item.get("longitude", None))
-
-
-        cursor.execute(
-            "INSERT INTO dublinbike_static (number,	name,	address,	latitude,	longitude) VALUES (%s,	%s,	%s,	%s,	%s)",    (number, name, address, latitude, longitude))
-    con.commit()
-    con.close()
-
-    # except:
-    #         print("Unable to get information.")
-    # return
-
+    while True:
+        # try:
+        now = datetime.datetime.now()
+        r = requests.get(STATIONS, params={"apiKey": APIKEY, "contract": NAME})
+        print(r, now)
+        write_to_file(r.text)
+        station_to_db(r.text)
+        availability_to_db(r.text)
+        time.sleep(5)
+        # except:
+        #     print(traceback.format_exc())
+    return
 
 if __name__ == '__main__':
     main()
-
