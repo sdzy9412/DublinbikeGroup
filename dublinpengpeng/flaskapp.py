c5e80be07b7d2f17a2efc067581d3914e4f54c9d
RubyJ <xi.jiang@ucdconnect.ie>
2020-02-25 11:27:15 +0000

    build framework of flask/html file (with errors passing list variables to initMap() function)

---
+++ dublinpengpeng/flaskapp.py
@@ -0,0 +1,32 @@
+'''environment variable setting in terminal:
+
+export FLASK_APP = flaskapp.py
+flask run
+export FLASK_DEBUG = 1 (set flask to debug mode)
+'''
+
+from flask import Flask, render_template
+import json
+app = Flask(__name__)
+
+# class Station:
+#     def __init__(self, num, name, lat, lng):
+#         self.num  = num
+#         self.name = name
+#         self.lat  = lat
+#         self.lng  = lng
+
+with open("dublin.json") as json_data:
+    station_file = json.load(json_data)
+
+bike_station = []
+
+for station in station_file:
+    bike_station.append([station.get('number'), station.get('name'), station.get('latitude'), station.get('longitude')])
+
+@app.route("/")
+def index():
+    return render_template("index.html", bike_station=bike_station)
+
+if __name__ == '__main__':
+    app.run(host="localhost", debug=True)
