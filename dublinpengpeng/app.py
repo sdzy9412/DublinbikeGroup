35293530233666b63876b6ae80526b193cca34c0
yi <sdzy9412@gmail.com>
2020-03-01 00:04:21 +0000

    json from DB parse json in index.html to show map and marker

--- dublinpengpeng/app.py
+++ dublinpengpeng/app.py
@@ -9,26 +9,15 @@ from flask import Flask, render_template, jsonify
 import pymysql
 
 app = Flask(__name__,static_url_path='')
-# app.config.from_object('config')
+
 
 def connect_to_database():
     conn = pymysql.connect(host='dublinbike.cx8vz93uf5cv.eu-west-1.rds.amazonaws.com', user='admin', passwd='pengpeng',db='dublinbike')
     return conn
 
-# with open("dublin.json") as json_data:
-#     station_file = json.load(json_data)
-#
-# bike_station = []
-#
-# for station in station_file:
-#     bike_station.append([station.get('number'), station.get('name'), station.get('latitude'), station.get('longitude')])
-# length = len(bike_station)
-
 @app.route("/")
 def index():
-    # return render_template("index.html", bike_station=bike_station, length=length)
-    #return render_template('index.html',MAPS_APIKEY = app.config["MAPS_APIKEY"])
-    return render_template('showmarker.html')
+    return render_template('index.html')
 
 @app.route("/stations", methods=['GET', 'POST'])
 def get_station():
