ae6a52d47ddd401f54335ca23e8b4791b28b4c3f
RubyJ <xi.jiang@ucdconnect.ie>
2020-03-02 17:04:29 +0000

    Delete data directory

--- dublinpengpeng/JsontoMysql.py
+++ dublinpengpeng/JsontoMysql.py
@@ -64,14 +64,6 @@ def main():
     engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
     engine.execute(SQL_CREATE_DB)
 
-    # Create target Directory if don't exist
-    if not os.path.exists("data"):
-        os.mkdir("data")
-
-    def write_to_file(text):
-        with open("data/bikes_{}".format(now).replace(" ", "_"), "w") as f:
-            f.write(text)
-
     def station_to_db(text):
         try:
             res = engine.execute("DROP TABLE IF EXISTS station")
@@ -113,7 +105,6 @@ def main():
         now = datetime.datetime.now()
         r = requests.get(STATIONS, params={"apiKey": APIKEY, "contract": NAME})
         print(r, now)
-        write_to_file(r.text)
         station_to_db(r.text)
         availability_to_db(r.text)
         time.sleep(30*60)
