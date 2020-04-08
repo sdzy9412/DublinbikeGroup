fcba508460ee7f9a4ee70f6f21724a24a0601beb
yi <sdzy9412@gmail.com>
2020-04-08 17:26:17 +0100

    predicted data with 3 hours interval

--- dublinpengpeng/app.py
+++ dublinpengpeng/app.py
@@ -66,16 +66,16 @@ class LoginForm(FlaskForm):
 def root():
     return render_template("homepage.html")
 
-@app.route("/", methods=["POST"])
-def send():
-    pick = request.form.get("pick")
-    pickdate = request.form.get("pickdate")
-    picktime = request.form.get("picktime")
-    drop = request.form.get("drop")
-    dropdate = request.form.get("dropdate")
-    droptime = request.form.get("droptime")
-    return render_template("homepage.html")
-    # return render_template("submit.html", pick=pick, drop=drop, pickdate=pickdate, picktime=picktime, dropdate=dropdate, droptime=droptime)
+# @app.route("/", methods=["POST"])
+# def send():
+#     pick = request.form.get("pick")
+#     pickdate = request.form.get("pickdate")
+#     picktime = request.form.get("picktime")
+#     drop = request.form.get("drop")
+#     dropdate = request.form.get("dropdate")
+#     droptime = request.form.get("droptime")
+#     return render_template("homepage.html")
+#     # return render_template("submit.html", pick=pick, drop=drop, pickdate=pickdate, picktime=picktime, dropdate=dropdate, droptime=droptime)
 
 @app.route("/uitest")
 def ui():
@@ -158,87 +158,105 @@ def get_weather():
 
     return jsonify(weather=weather)
 
-@app.route("/prediction_model", methods=['GET','POST'])
+@app.route("/", methods=['GET','POST'])
 def prediction_model():
-    #通过post得到所要的数据(如果是分开传？)
-    #第一部分：得到对应时间的预测天气
-    # post = request.args.get('id', 0, type=str) #如果是一起传过来的
-    # 或者：post = str(request.args.get('id', 0, type=str)) #post来的时间
-    # post = post.split() #如果split后把日期与时间分开 我再post[1]=post[1]+post[2]应该就可以
-    post = [42, "2020-04-08 02:00:00", 0]  # 只是测试 实际代码为下下一行
-    post = [42,"2020-04-08 02:00:00",0,31,"2020-04-08 09:00:00",1] # 只是测试 实际代码为下一行
+    pick = request.form.get("pick")
+    pickdate = request.form.get("pickdate")
+    picktime = request.form.get("picktime")
+    drop = request.form.get("drop")
+    dropdate = request.form.get("dropdate")
+    droptime = request.form.get("droptime")
+    print("picktime: ",picktime)
+    print("droptime: ", droptime)
+    if picktime == '':
+        print("end only")
+        post = [drop,dropdate+" "+droptime]
+    elif droptime == '':
+        print("start only")
+        post =  [pick,pickdate+" "+picktime]
+    elif picktime != '' and droptime != '':
+        print("both 2 stations")
+        post = [pick, pickdate + " " + picktime, drop, dropdate + " " + droptime]
+    # post = [14,'2020-04-11 12:13',42,'2020-04-13 02:03']
+    # post = [14,'2020-04-12 12:13']
+    print("post1:",post)
+
     def allweatherdata():
         url = 'http://api.openweathermap.org/data/2.5/forecast?q=Dublin,ie&units=metric&APPID=7c4d32959a99216eeb3c99efc8000278'  # 目前这个url可用
         weatherDataString = requests.get(url=url)
         allweather = weatherDataString.json()
         return allweather
 
-    def weatherdata(post):
+    def weatherdata(post,pick,pickdate, picktime,drop,dropdate,droptime):
         allweather = allweatherdata()
-        weatherdatalist = []
-        if(len(post)==3):
-            inputtime = datetime.strptime(post[1], '%Y-%m-%d %H:%M:%S')
-            three_hours_from_input = inputtime + timedelta(hours=3)
-            three_hours_to_input = inputtime - timedelta(hours=3)
-            print(three_hours_from_input)
-            print(three_hours_to_input)
-            print("以上是输入时间加减3小时")
-            for i in range(0, len(allweather['list'])):
-                time = allweather['list'][i]['dt_txt']
-                time_datetime = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
-                # type(time)
-                if (three_hours_from_input > time_datetime > three_hours_to_input):  # 目前只能做相等 还要改为大于小于
-                    print(i,time)
-                    temp = allweather['list'][i]['main']['temp']
-                    cloud = allweather['list'][i]['weather'][0]['main']
-                    speed = allweather['list'][i]['wind']['speed']
-                    weatherdatalist.extend((temp,cloud,speed))
-                    return weatherdatalist
-        elif(len(post)==6):
-            inputtime_start = datetime.strptime(post[1], '%Y-%m-%d %H:%M:%S')
-            inputtime_end = datetime.strptime(post[4], '%Y-%m-%d %H:%M:%S')
-            three_hours_from_start = inputtime_start + timedelta(hours=1.5)
-            three_hours_to_start = inputtime_start - timedelta(hours=1.5)
-            three_hours_from_end = inputtime_end + timedelta(hours=1.5)
-            three_hours_to_end = inputtime_end - timedelta(hours=1.5)
+        weatherdatalists = []
+        if(len(post)==2):
+            inputtime = datetime.strptime(post[1], '%Y-%m-%d %H:%M') - timedelta(hours=1)
+            for k in range(3):
+                weatherdatalist = []
+                print(k)
+                inputtime = inputtime + timedelta(hours=k)
+                # print("inputtime: ",inputtime)
+                three_hours_from_input = inputtime + timedelta(hours=1.5)
+                three_hours_to_input = inputtime - timedelta(hours=1.5)
+                for i in range(0, len(allweather['list'])):
+                    time = allweather['list'][i]['dt_txt']
+                    time_datetime = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+                    if (three_hours_from_input > time_datetime > three_hours_to_input):  # 目前只能做相等 还要改为大于小于
+                        print("only start or end：",i,time)
+                        temp = allweather['list'][i]['main']['temp']
+                        cloud = allweather['list'][i]['weather'][0]['main']
+                        speed = allweather['list'][i]['wind']['speed']
+                        weatherdatalist.extend((str(inputtime),temp,cloud,speed))
+                weatherdatalists.append(weatherdatalist)
+                # print("加s:",weatherdatalists)
+            return weatherdatalists
+        elif(len(post)==4):
+            inputtime_start = datetime.strptime(post[1], '%Y-%m-%d %H:%M') - timedelta(hours=1)
+            print("post[3] is : ",post[3])
+            inputtime_end = datetime.strptime(post[3], '%Y-%m-%d %H:%M') - timedelta(hours=1)
+            for k in range(3):
+                weatherdatalist = []
+                inputtime_start = inputtime_start+ timedelta(hours=k)
+                inputtime_end = inputtime_end + timedelta(hours=k)
+                three_hours_from_start = inputtime_start + timedelta(hours=1.5)
+                three_hours_to_start = inputtime_start - timedelta(hours=1.5)
+                three_hours_from_end = inputtime_end + timedelta(hours=1.5)
+                three_hours_to_end = inputtime_end - timedelta(hours=1.5)
 
-            for i in range(0, len(allweather['list'])):
-                time = allweather['list'][i]['dt_txt']
-                time_datetime = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
-                # type(time)
-                if (three_hours_from_start > time_datetime > three_hours_to_start):
-                    print("start:", i, time)
-                    temp = allweather['list'][i]['main']['temp']
-                    cloud = allweather['list'][i]['weather'][0]['main']
-                    speed = allweather['list'][i]['wind']['speed']
-                    weatherdatalist.extend((temp, cloud, speed))
-            for i in range(0, len(allweather['list'])):
-                time = allweather['list'][i]['dt_txt']
-                time_datetime = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
-                # type(time)
-                if (three_hours_from_end > time_datetime > three_hours_to_end):
-                    print("end",i, time)
-                    temp = allweather['list'][i]['main']['temp']
-                    cloud = allweather['list'][i]['weather'][0]['main']
-                    speed = allweather['list'][i]['wind']['speed']
-                    weatherdatalist.extend((temp, cloud, speed))
-            return weatherdatalist
+                for i in range(0, len(allweather['list'])):
+                    time = allweather['list'][i]['dt_txt']
+                    time_datetime = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+                    if (three_hours_from_start > time_datetime > three_hours_to_start):
+                        print("start:", i, time)
+                        temp = allweather['list'][i]['main']['temp']
+                        cloud = allweather['list'][i]['weather'][0]['main']
+                        speed = allweather['list'][i]['wind']['speed']
+                        weatherdatalist.extend((str(inputtime_start),temp, cloud, speed))
+                        print("无s——start",weatherdatalist)
+                for i in range(0, len(allweather['list'])):
+                    time = allweather['list'][i]['dt_txt']
+                    time_datetime = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+                    if (three_hours_from_end > time_datetime > three_hours_to_end):
+                        print("end",i, time)
+                        temp = allweather['list'][i]['main']['temp']
+                        cloud = allweather['list'][i]['weather'][0]['main']
+                        speed = allweather['list'][i]['wind']['speed']
+                        weatherdatalist.extend((str(inputtime_end),temp, cloud, speed))
+                        print("无s——end",weatherdatalist)
+                weatherdatalists.append(weatherdatalist)
+                print("加s:",weatherdatalists)
+            return weatherdatalists
 
-    weatherdatalist = weatherdata(post)
-    print(weatherdatalist)
+    weatherdatalists = weatherdata(post,pick,pickdate, picktime,drop,dropdate,droptime)
+    print("final check weatherdatalists：",weatherdatalists)
 
     #以下第二部分：预测数据
-    #传入信息格式：[num,time,0/1]
-    # eg:只有起点[42,"2020-04-08 02:00:00",0] 只有终点同理将0改为1
-    # 起点终点都有[42,"2020-04-08 02:00:00",0,31,"2020-04-08 09:00:00",1]
     random_forest_bikes = pickle.load(open('final_prediction_bike.pickle', 'rb'))
     random_forest_stands = pickle.load(open("final_prediction_bike_stands.pickle", "rb"))
-    # post = request.args.get('id', 0, type=str)
-    # post = post.split()
-    post.extend(weatherdatalist)
+    # post.extend(weatherdatalists)
     result = []
-    #上一步的结果：0起点站 1开始时间 2标志1 3终点站 4结束时间 5标志2 6气温1 7天气1 8风速1 9气温2 10天气2 11风速2
-    # post = [42, '2020-04-08 02:00:00', 0, 31, '2020-04-08 09:00:00', 1, 5.8, 'Clear', 3.56, 9.56, 'Clouds', 2.16]
+
     print("post:",post)
 
     def weekday_weather(weekday, weather):
@@ -270,70 +288,71 @@ def prediction_model():
             mist = 1
         weatherwithweekdy=[friday, monday, saturday, sunday, thursday, tuesday, wednesday, clear, clouds, drizzle, mist, rain]
         return (weatherwithweekdy)
+    #
+    for weatherdata in weatherdatalists:
+        print(weatherdata)
+        if(len(weatherdata)==8):#起始站都有
+            print("enter this-2 station:")
+            stationNum_start = int(post[0])
+            inputtime_start = datetime.strptime(weatherdata[0], '%Y-%m-%d %H:%M:%S')
+            weekday_start = inputtime_start.weekday()
+            hour_start = int(inputtime_start.hour)
+            stationNum_end = int(post[2])
+            inputtime_end = datetime.strptime(weatherdata[4], '%Y-%m-%d %H:%M:%S')
+            weekday_end = inputtime_end.weekday()
+            hour_end = int(inputtime_end.hour)
+            temp_start = float(weatherdata[1])
+            weather_start = weatherdata[2]
+            windSpeed_start = float(weatherdata[3])
+            temp_end = float(weatherdata[5])
+            weather_end = weatherdata[6]
+            windSpeed_end = float(weatherdata[7])
+            datalist_start = weekday_weather(weekday_start,weather_start)
+            datalist_end = weekday_weather(weekday_end,weather_end)
 
-    if(len(post)==12):
-        print("enter this:")
-        # post = [42, "2020-04-08 02:00:00", 0, 31, "2020-04-08 09:00:00", 1]
-        stationNum_start = int(post[0])
-        inputtime_start = datetime.strptime(post[1], '%Y-%m-%d %H:%M:%S')
-        weekday_start = inputtime_start.weekday()
-        hour_start = int(inputtime_start.hour)
-        stationNum_end = int(post[3])
-        inputtime_end = datetime.strptime(post[4], '%Y-%m-%d %H:%M:%S')
-        weekday_end = inputtime_end.weekday()
-        hour_end = int(inputtime_end.hour)
-        temp_start = float(post[6])
-        weather_start = post[7]
-        windSpeed_start = float(post[8])
-        temp_end = float(post[9])
-        weather_end = post[10]
-        windSpeed_end = float(post[11])
-        datalist_start = weekday_weather(weekday_start,weather_start)
-        datalist_end = weekday_weather(weekday_end,weather_end)
 
-
-        #starting station
-        predict_request = [[stationNum_start,temp_start,windSpeed_start,hour_start,datalist_start[0],datalist_start[1],datalist_start[2],datalist_start[3],datalist_start[4],datalist_start[5],datalist_start[6],datalist_start[7],datalist_start[8],datalist_start[9],datalist_start[10],datalist_start[11]]]
-        available_bikes_P = random_forest_bikes.predict(predict_request)
-        available_bikes_P = [int(available_bikes_P)]
-        result.extend(available_bikes_P)
-        #destination
-        predict_request = [[stationNum_end,temp_end,windSpeed_end,hour_end,datalist_end[0],datalist_end[1],datalist_end[2],datalist_end[3],datalist_end[4],datalist_end[5],datalist_end[6],datalist_end[7],datalist_end[8],datalist_end[9],datalist_end[10],datalist_end[11]]]
-        available_stands_P = random_forest_stands.predict(predict_request)
-        available_stands_P = [int(available_stands_P)]
-        result.extend(available_stands_P)
-    # post = [42, '2020-04-08 02:00:00', 0, 5.8, 'Clear', 3.56]
-    elif(len(post)==6):
-        stationNum = int(post[0])
-        inputtime= datetime.strptime(post[1], '%Y-%m-%d %H:%M:%S')
-        weekday = inputtime.weekday()
-        hour = int(inputtime.hour)
-        flag = int(post[2])  #
-        temp = float(post[3])
-        weather = post[4]
-        windSpeed= float(post[5])
-        datalist = weekday_weather(weekday, weather)
-        if(flag == 0):
-            # starting station
-            predict_request = [
-                [stationNum, temp, windSpeed, hour, datalist[0], datalist[1], datalist[2],
-                 datalist[3], datalist[4], datalist[5], datalist[6], datalist[7], datalist[8], datalist[9],
-                 datalist[10], datalist[11]]]
+            #starting station
+            predict_request = [[stationNum_start,temp_start,windSpeed_start,hour_start,datalist_start[0],datalist_start[1],datalist_start[2],datalist_start[3],datalist_start[4],datalist_start[5],datalist_start[6],datalist_start[7],datalist_start[8],datalist_start[9],datalist_start[10],datalist_start[11]]]
             available_bikes_P = random_forest_bikes.predict(predict_request)
             available_bikes_P = [int(available_bikes_P)]
             result.extend(available_bikes_P)
-        elif(flag==1):
-            # destination
-            predict_request = [
-                [stationNum, temp, windSpeed, hour, datalist[0], datalist[1], datalist[2],
-                 datalist[3], datalist[4], datalist[5], datalist[6], datalist[7], datalist[8], datalist[9],
-                 datalist[10], datalist[11]]]
+            #destination
+            predict_request = [[stationNum_end,temp_end,windSpeed_end,hour_end,datalist_end[0],datalist_end[1],datalist_end[2],datalist_end[3],datalist_end[4],datalist_end[5],datalist_end[6],datalist_end[7],datalist_end[8],datalist_end[9],datalist_end[10],datalist_end[11]]]
             available_stands_P = random_forest_stands.predict(predict_request)
             available_stands_P = [int(available_stands_P)]
             result.extend(available_stands_P)
 
-    result.extend(weatherdatalist)
-    return jsonify(result)
+        elif(len(weatherdata)==4):#只有开始或结束
+            print("enter this-1 station:")
+            stationNum = int(post[0])
+            inputtime= datetime.strptime(weatherdata[0], '%Y-%m-%d %H:%M:%S')
+            weekday = inputtime.weekday()
+            hour = int(inputtime.hour)
+            temp = float(weatherdata[1])
+            weather = weatherdata[2]
+            windSpeed= float(weatherdata[3])
+            datalist = weekday_weather(weekday, weather)
+            if(pick != None):
+                # starting station
+                predict_request = [
+                    [stationNum, temp, windSpeed, hour, datalist[0], datalist[1], datalist[2],
+                     datalist[3], datalist[4], datalist[5], datalist[6], datalist[7], datalist[8], datalist[9],
+                     datalist[10], datalist[11]]]
+                available_bikes_P = random_forest_bikes.predict(predict_request)
+                available_bikes_P = [int(available_bikes_P)]
+                result.extend(available_bikes_P)
+            elif(drop != None):
+                # destination
+                predict_request = [
+                    [stationNum, temp, windSpeed, hour, datalist[0], datalist[1], datalist[2],
+                     datalist[3], datalist[4], datalist[5], datalist[6], datalist[7], datalist[8], datalist[9],
+                     datalist[10], datalist[11]]]
+                available_stands_P = random_forest_stands.predict(predict_request)
+                available_stands_P = [int(available_stands_P)]
+                result.extend(available_stands_P)
+
+    print("final result--",result)
+    return render_template("homepage.html", preresult=result)
 
 
 
